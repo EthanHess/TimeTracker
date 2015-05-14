ff2e56e65c3a7176f202118f0c475134999e0e18
EthanHess <ech1988@gmail.com>
2015-05-14 14:18:21 -0600

    initial commit

---
+++ Time Tracker/Time Tracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Time Tracker
+//
+//  Created by Ethan Hess on 5/14/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Time Tracker/Time Tracker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Time Tracker
+//
+//  Created by Ethan Hess on 5/14/15.
+//  Copyright (c) 2015 Ethan Hess. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
