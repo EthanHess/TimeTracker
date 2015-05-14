8a4cf54631f406dcd2a5c8e5cc9a07ec82f9ec6d
EthanHess <ech1988@gmail.com>
2015-05-14 17:05:03 -0600

    Sets Email message Compose & textfield delegate

--- Time Tracker/Time Tracker/AddEntryViewController.m
+++ Time Tracker/Time Tracker/AddEntryViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "AddEntryViewController.h"
+#import "ProjectController.h"
+#import "Project.h"
 
 @interface AddEntryViewController () <UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *projectNameTextField;
 @property (weak, nonatomic) IBOutlet UILabel *clockinLabel;
 @property (weak, nonatomic) IBOutlet UILabel *clockoutLabel;
+@property (nonatomic, strong) Project *project;
 
 @end
 
@@ -22,9 +25,12 @@
     [super viewDidLoad];
     
     self.title = @"New Entry";
+    self.projectNameTextField.delegate = self; 
 }
 
 - (IBAction)saveButton:(id)sender {
+    
+    [[ProjectController sharedInstance]addProject:self.project];
     
 }
 
@@ -38,6 +44,19 @@
 
 - (IBAction)clockoutPicker:(id)sender {
     
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+//set the project.title to the textFields.text
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField{
+    self.project.title = textField.text;
+    [[ProjectController sharedInstance] save];
+    
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning {
--- Time Tracker/Time Tracker/ListViewController.m
+++ Time Tracker/Time Tracker/ListViewController.m
@@ -7,10 +7,15 @@
 //
 
 #import "ListViewController.h"
+#import "Entry.h"
+#import "Project.h"
+#import "ProjectController.h"
+@import MessageUI;
 
-@interface ListViewController () <UITableViewDelegate>
+@interface ListViewController () <UITableViewDelegate, MFMailComposeViewControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableview;
+@property (nonatomic, weak) Project *project;
 
 @end
 
@@ -21,6 +26,47 @@
     
     
     
+}
+
+- (IBAction)clockinButton:(id)sender {
+    
+//    [self.project startNewEntry];
+//    [self.tableview reloadData];
+}
+
+- (IBAction)clockoutButton:(id)sender {
+    
+//    [self.project endCurrentEntry];
+//    [self.tableview reloadData];
+}
+
+- (IBAction)reportButton:(id)sender {
+    
+    MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc] init];
+    mailViewController.mailComposeDelegate = self;
+    
+    //Create a message body
+    NSString *messageBody;
+    
+    for (Entry *entry in self.project.entries) {
+        
+        if (messageBody) {
+            messageBody = [NSString stringWithFormat:@"%@\n%@ to %@\n", messageBody, entry.startTime, entry.endTime];
+        } else {
+            messageBody = [NSString stringWithFormat:@"\n%@ to %@\n", entry.startTime, entry.endTime];
+            
+        }
+    }
+    
+    //set the message body to the message body string
+    [mailViewController setMessageBody:messageBody isHTML:NO];
+    
+    [self presentViewController:mailViewController animated:YES completion:nil];
+}
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
